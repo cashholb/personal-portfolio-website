c7896c54421eb26129561fbd4917f14737ed7d45
cashtonholbert <cashtoncoleholbert@gmail.com>
2024-01-22 18:28:53 -0800

    fixed svg path bug for deployed website

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,15 +1,21 @@
+import { lazy, Suspense } from 'react';
 import './hero.scss';
-import HeroImage from './heroImage/HeroImage';
+
 import HeroText from './heroText/HeroText';
 
-function Hero() {
+const LazyHeroImage = lazy(() => import('./heroImage/HeroImage'));
 
+function Hero() {
   return (
     <div className="hero">
-      <HeroImage></HeroImage>
-      <HeroText></HeroText>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyHeroImage />
+      </Suspense>
+      <HeroText />
     </div>
   );
 }
 
 export default Hero;
+
+
--- src/components/hero/heroImage/StencilAnimation/AnimatedSVG.jsx
+++ src/components/hero/heroImage/StencilAnimation/AnimatedSVG.jsx
@@ -1,55 +1,42 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import useMediaQuery from '../../../hooks/useMediaQuery';
 
-const AnimatedSVG = ({ duration = 6, svgPathParam = '/heroImage/svgPath.txt', width=444, height=600}) => {
-
+const AnimatedSVG = ({ duration = 12, svgPathParam = "", width = 444, height = 600 }) => {
   const isAnimatedSVGTooBig = useMediaQuery('(max-width: 1140px)');
-  const [svgPath, setSvgPath] = useState('');
   const controls = useAnimation();
 
   const wrapperVariants = {
     hidden: {
-      clipPath: "circle(1200px at 50px 50px)",
+      clipPath: 'circle(1200px at 50px 50px)',
       pathLength: 0,
     },
     visible: {
-      clipPath: "circle(1200px at 50px 50px)",
+      clipPath: 'circle(1200px at 50px 50px)',
       pathLength: 1,
       transition: { duration: duration, ease: 'easeInOut' },
     },
     exit: {
       pathLength: 0,
-    }
-  }
-
-  // Fetch SVG path from the text file
-  useEffect(() => {
-    const fetchSvgPath = async () => {
-      try {
-        const response = await fetch(svgPathParam);
-        console.log(response);
-        const pathData = await response.text();
-        setSvgPath(pathData);
-      } catch (error) {
-        console.error('Error fetching SVG path:', error);
-      }
-    };
-    fetchSvgPath();
-  }, []);
+    },
+  };
 
   // Trigger animation on mount
   useEffect(() => {
     controls.start('visible');
-  }, [controls]);
+  }, []);
 
   return (
     <>
-      <motion.svg xmlns="http://www.w3.org/2000/svg"  width={isAnimatedSVGTooBig ? `${width * 0.75}` : `${width}`} height={isAnimatedSVGTooBig ? `${height * 0.75}` : `${height}`}>
+      <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={isAnimatedSVGTooBig ? `${width * 0.75}` : `${width}`}
+        height={isAnimatedSVGTooBig ? `${height * 0.75}` : `${height}`}
+      >
         <motion.path
-          transform={isAnimatedSVGTooBig ? "scale(0.75)" : "scale(1)"}
-          d={`${svgPath}`}
+          transform={isAnimatedSVGTooBig ? 'scale(0.75)' : 'scale(1)'}
+          d={svgPathParam}
           fill="none"
           stroke="white"
           strokeWidth="2"
@@ -59,7 +46,6 @@ const AnimatedSVG = ({ duration = 6, svgPathParam = '/heroImage/svgPath.txt', wi
           exit="exit"
         />
       </motion.svg>
-
     </>
   );
 };
@@ -69,6 +55,6 @@ AnimatedSVG.propTypes = {
   svgPathParam: PropTypes.string,
   width: PropTypes.number,
   height: PropTypes.number,
-}
+};
 
 export default AnimatedSVG;
